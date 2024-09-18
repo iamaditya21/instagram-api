b8b5145a0a27b15bc4068dfae83437f4d73ed75b
iamaditya21 <97442241+iamaditya21@users.noreply.github.com>
2024-09-18 16:25:22 +0530

    go.mod file modifies

--- instagram-api/handlers/users_handlers.go
+++ instagram-api/handlers/users_handlers.go
@@ -4,7 +4,7 @@ import (
     "net/http"
     "github.com/gin-gonic/gin"
     "golang.org/x/crypto/bcrypt"
-    "github.com/yourusername/instagram-api/database"
+    "github.com/iamaditya21/instagram-api/instagram-api/database"
 )
 
 func CreateUser(c *gin.Context) {
